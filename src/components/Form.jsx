ff1ec7b3eaf305b64efcfc46e0ffe1e7a2ebbac0
Vladimir Spektor <viaes88@gmail.com>
2025-12-17 14:04:25 +0200

    main

---
+++ src/components/Form.jsx
@@ -0,0 +1,17 @@
+const Form = ({getWeather}) => {
+
+    const handleClickSubmit = e => {
+        e.preventDefault();
+        const city = e.currentTarget.city.value.trim();
+        getWeather(city);
+    }
+
+    return (
+        <form onSubmit={handleClickSubmit}>
+            <input type={"text"} name={'city'}/>
+            <button type={'submit'}>Get Weather</button>
+        </form>
+    )
+}
+
+export default Form;
